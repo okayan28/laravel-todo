5cfbcff12492fde6db772e098c55708688214d1a
okayan28 <xxxx.xoxox@gmail.com>
2021-11-11 09:02:01 -0500

    タスクを保存できるようにしている途中

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,12 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\task;
 
 class TaskController extends Controller
 {
     public function index(Request $request)
     {
     return view('task.index');
+    }
+
+    public function add(Request $request)
+    {
+        return view('task.add');
+    }
+
+    public function create(Request $request)
+    {
+        $this->validate($request, Task::$rules);
+        $task = new Task;
+        $form = $request->all();
+        unset($form['_token']);
+        $task->fill($form)->save();
+        return redirect('/task');
     }
 }
 
--- app/Models/task.php
+++ app/Models/task.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class task extends Model
 {
     use HasFactory;
+
+    protected $guarded = array('id');
+
+    public static $rules = array(
+        'title' => 'required|string|max:30',
+        'description' => 'required|string|max:180',
+        'deadline' => 'after:now'
+    );
+
+    public function getData()
+    {
+        return $this->id . ':' . $this->title . '/' . $this->description . '(' . $this->deadline . ')';
+    }
 }
